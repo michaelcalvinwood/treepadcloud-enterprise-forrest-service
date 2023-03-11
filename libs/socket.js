b0173c6eae86a7ef76caec6eeb2faadb3a45d2b4
Michael Wood <michaelwood33311@icloud.com>
2023-03-11 11:31:54 -0500

    added plumbing for handling subscription requests

--- libs/socket.js
+++ libs/socket.js
@@ -1,5 +1,20 @@
 
+const subscribe = (socket, resource, token) => {
+    console.log('subscribe event');
+    return new Promise((resolve, reject) => {
+        socket.emit('subscribe', 'success');
+
+        resolve('ok');
+    })
+}
+
+const handleSocketEvents = socket => {
+    socket.on('subscribe', (resource, token) => subscribe(socket, resource, token));
+}
 
 exports.handleConnection = socket => {
     console.log(socket.id);
+    socket.emit('hello', socket.id);
+
+    handleSocketEvents(socket);
 }
